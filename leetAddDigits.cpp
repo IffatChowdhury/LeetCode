59dd2fd28a87a9ad7b71f5f8457e20442bc3a850
Iffat Chowdhury <iffat.chowdhury@utsa.edu>
2016-02-09 23:46:23 -0600

    Update leetAddDigits.cpp

--- leetAddDigits.cpp
+++ leetAddDigits.cpp
@@ -1,5 +1,6 @@
 /*
 Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+Without using loop / recursion, just use n - 9 floor( (n-1)/9 )
 */
 
 class Solution {
